45391c91831a2fb2fcf6ec85c6d2ab00f08cc638
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-30 17:54:16 -0500

    Fix linters

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ class V1::GreetingsController < ApplicationController
   def index
     greeting = Greeting.find(Greeting.pluck(:id).sample)
     render json: {
-      :greeting => greeting.text,
+      greeting: greeting.text
     }.to_json
   end
 end
